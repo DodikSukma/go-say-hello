d5abc64fc3551821e94eb9b09533c49d828749bc
dodik_sukma <putudodik2000@gmail.com>
2025-01-31 22:58:04 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/DodikSukma/go-say-hello
+module github.com/DodikSukma/go-say-hello/v2
 
 go 1.23.5
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello, Go Dodik Anjay WUUUK!"
+func SayHello(name string) string {
+	return "Hello : " + name
 }
